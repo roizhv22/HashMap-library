18f8b8189183267e334d066f8aa6574f9c9420d5
roizhv22 <roi.zehavi@mail.huji.ac.il>
2021-05-20 21:57:09 +0300

    after debug and mem-leak checks

--- test_suite.c
+++ test_suite.c
@@ -4,11 +4,12 @@
 #include "hash_funcs.h"
 #include "pair.h"
 
-#define NUM_OF_PAIRS 200 //careful from char overflow as some
+#define NUM_OF_CHAR_INT_PAIRS 200 //careful from char overflow as some
 //functions checks the char pairs and we can only have 256 keys.
+#define NUM_OF_INT_FLOAT_PAIRS 100000
 #define INT_KEY_BASE_VALUE 100
 #define FLOAT_VALUE_BASE_VAL 50.5
-#define DELTA_FOR_FLOAT_VAL 6.6
+#define DELTA_FOR_FLOAT_VAL 6.68
 #define INT_VALUE_BASE 500
 #define INT_VALUE_DELTA 30
 #define CHAR_KEY_BASE 10
@@ -18,14 +19,14 @@
  * creates MUM_OF_PAIRS int-float general pairs. FREE NEEDED!
  * returns NULL if malloc fails.
  */
-pair** create_int_float_pairs(){
-  pair** pairs = malloc (sizeof (pair*)*NUM_OF_PAIRS);
+pair** create_int_float_pairs(size_t num_of_pairs){
+  pair** pairs = malloc (sizeof (pair*)*num_of_pairs);
   if (pairs == NULL){
       return NULL;
   }
   int base_key = INT_KEY_BASE_VALUE;
   float base_val = FLOAT_VALUE_BASE_VAL;
-  for (int i = 0; i < NUM_OF_PAIRS; ++i)
+  for (size_t i = 0; i < num_of_pairs; ++i)
     {
       pair *pr = pair_alloc (&base_key, &base_val, int_key_cpy,float_value_cpy,
                              int_key_cmp, float_value_cmp,
@@ -40,13 +41,13 @@ pair** create_int_float_pairs(){
  * creates NUM_OF_PAIRS char-int pairs. returns the array - FREE NEEDED!
  * NULL ptr will be returned if malloc fails.
  */
-pair** create_char_int_pairs(){
-  pair** pairs = malloc (sizeof (pair*)*NUM_OF_PAIRS);
+pair** create_char_int_pairs(size_t num_of_pairs){
+  pair** pairs = malloc (sizeof (pair*)*num_of_pairs);
   if (pairs == NULL){
       return NULL;
   }
   int base_val = INT_VALUE_BASE;
-  for (int i = 0; i < NUM_OF_PAIRS; ++i)
+  for (size_t i = 0; i < num_of_pairs; ++i)
     {
       char base_key = (char) (i+CHAR_KEY_BASE);
       pair *pr = pair_alloc (&base_key, &base_val, char_key_cpy,int_value_cpy,
@@ -61,8 +62,8 @@ pair** create_char_int_pairs(){
  * frees the pairs! number of pairs to be free is given in the marco NUM
  * OF PAIRS
  */
-void free_pair_list(pair*** pairs){
-  for (int i = 0; i <NUM_OF_PAIRS ; ++i)
+void free_pair_list(pair*** pairs, size_t num_of_pairs){
+  for (size_t i = 0; i <num_of_pairs ; ++i)
     {
       pair_free((void**)&((*pairs)[i]));
     }
@@ -74,10 +75,10 @@ void free_pair_list(pair*** pairs){
  * general insertion test. performing assert checks on different scenarios.
  * due note that this function frees the hash_map and pairs!
  */
-void general_insert_test (hashmap* hash_map,pair** pairs)
+void general_insert_test (hashmap* hash_map,pair** pairs, size_t num_of_pairs)
 {
   size_t old_capacity = hash_map->capacity;
-  for (size_t  i = 0; i < NUM_OF_PAIRS; ++i)
+  for (size_t i = 0; i < num_of_pairs; ++i)
     {
       assert(hashmap_insert (hash_map, pairs[i]));
       assert(hash_map->size == i+1); // checks size increase correctly.
@@ -86,19 +87,19 @@ void general_insert_test (hashmap* hash_map,pair** pairs)
       if (prev_load>HASH_MAP_MAX_LOAD_FACTOR){
           assert(hash_map->capacity == old_capacity*HASH_MAP_GROWTH_FACTOR);
           old_capacity = hash_map->capacity;
-          //checks that the capacity increase is performing as exepected.
+          //checks that the capacity increase is performing as expected.
       }
     }
   size_t hash_size = hash_map->size;
   size_t hash_capacity = hash_map->capacity;
-  for (int i = 0; i < NUM_OF_PAIRS/4; ++i)
+  for (size_t i = 0; i < num_of_pairs/4; ++i)
     {
       assert(hashmap_insert (hash_map, NULL) == 0);
       assert(hash_map->size == hash_size);
       assert(hash_map->capacity == hash_capacity);
       // trying to enter NULL ptr.
     }
-  for (int i = 0; i < 50; ++i)
+  for (size_t i = 0; i < 50; ++i)
     {
       assert(hashmap_insert (hash_map, pairs[50+i])==0);
       assert(hash_map->size == hash_size);
@@ -108,7 +109,7 @@ void general_insert_test (hashmap* hash_map,pair** pairs)
       // make sure no changes to the size/capacity are made. 
     }
   hashmap_free (&hash_map);
-  free_pair_list (&(pairs));
+  free_pair_list (&(pairs), num_of_pairs);
 }
 
 /**
@@ -116,50 +117,52 @@ void general_insert_test (hashmap* hash_map,pair** pairs)
  * If hashmap_insert fails at some points, the functions exits with exit code 1.
  */
 void test_hash_map_insert(void){
-  pair** char_int_pairs = create_char_int_pairs();
+  pair** char_int_pairs = create_char_int_pairs(NUM_OF_CHAR_INT_PAIRS);
   if (char_int_pairs == NULL){
       exit (1); // malloc fails.
     }
   hashmap * char_int_hash = hashmap_alloc (hash_char);
   if (char_int_hash == NULL){
-      free_pair_list (&char_int_pairs);
+      free_pair_list (&char_int_pairs, NUM_OF_CHAR_INT_PAIRS);
       exit(1); // malloc fails.
     }
-  general_insert_test (char_int_hash, char_int_pairs);
+    //creates resources for the test.
+  general_insert_test (char_int_hash, char_int_pairs, NUM_OF_CHAR_INT_PAIRS);
   
-  pair** int_float_pairs = create_int_float_pairs();
+  pair** int_float_pairs = create_int_float_pairs(NUM_OF_INT_FLOAT_PAIRS);
   if (int_float_pairs == NULL){
       exit (1); // malloc fails.
     }
   hashmap * int_float_hash = hashmap_alloc (hash_int);
   if (int_float_hash == NULL){
-      free_pair_list (&int_float_pairs);
+      free_pair_list (&int_float_pairs, NUM_OF_INT_FLOAT_PAIRS);
       exit(1); // malloc fails.
     }
-  general_insert_test (int_float_hash, int_float_pairs);//the test functions
-  //free the hash_map and pairs!
+  //creates resources for the test.
+  general_insert_test (int_float_hash, int_float_pairs,NUM_OF_INT_FLOAT_PAIRS);
+  //the test function frees the hash_map and pairs!
 }
 /*
  * @param: receives an empty hash_map that is initialize to the pairs
  * type and a pairs list.
- * general testing on the at, checking invalid input and esixting
+ * general testing on the at, checking invalid input and existing
  * of a given key in the table.
  */
-void general_at_test(hashmap* hash_map, pair** pairs){
-  for (int i = 0; i < NUM_OF_PAIRS/2; ++i)
+void general_at_test(hashmap* hash_map, pair** pairs, size_t num_of_pairs){
+  for (size_t i = 0; i < num_of_pairs/2; ++i)
     {
       hashmap_insert (hash_map, pairs[i]); //insert half of the pairs.
     }
-  for (int i = 0; i < (int) NUM_OF_PAIRS/4; ++i)
+  for (size_t i = 0; i < num_of_pairs/4; ++i)
     {
       assert(hashmap_at (hash_map,NULL) == NULL);
       assert((hashmap_at (NULL, pairs[i]->key)) == NULL);
       assert((hashmap_at (NULL, NULL)) == NULL);
       //checks invalid input.
     }
-  for (int i = 0; i < NUM_OF_PAIRS; ++i)
+  for (size_t i = 0; i < num_of_pairs; ++i)
     {
-      if (i<NUM_OF_PAIRS/2){
+      if (i<num_of_pairs/2){
           assert((hashmap_at (hash_map, pairs[i]->key)) != NULL);
           //checks on pair that in the table.
       }
@@ -169,16 +172,17 @@ void general_at_test(hashmap* hash_map, pair** pairs){
       }
     }
   hashmap_free(&hash_map);
-  free_pair_list(&pairs);
+  free_pair_list(&pairs, num_of_pairs);
 }
 
 /**
  * This function checks the hashmap_at function of the hashmap library.
  * If hashmap_at fails at some points, the functions exits with exit code 1.
+ * The function perform two major tests on different pair input-types.
  */
 void test_hash_map_at(void)
 {
-  pair **char_int_pairs = create_char_int_pairs ();
+  pair **char_int_pairs = create_char_int_pairs (NUM_OF_CHAR_INT_PAIRS);
   if (char_int_pairs == NULL)
     {
       exit (1); // malloc fails.
@@ -186,11 +190,12 @@ void test_hash_map_at(void)
   hashmap *char_int_hash = hashmap_alloc (hash_char);
   if (char_int_hash == NULL)
     {
-      free_pair_list (&char_int_pairs);
+      free_pair_list (&char_int_pairs, NUM_OF_CHAR_INT_PAIRS);
       exit (1); // malloc fails.
     }
-  general_at_test (char_int_hash, char_int_pairs);//frees resources.
-  pair **int_float_pairs = create_int_float_pairs();
+  general_at_test (char_int_hash, char_int_pairs, NUM_OF_CHAR_INT_PAIRS);
+  //frees resources.
+  pair **int_float_pairs = create_int_float_pairs(NUM_OF_INT_FLOAT_PAIRS);
   if (int_float_pairs == NULL)
     {
       exit (1); // malloc fails.
@@ -198,10 +203,11 @@ void test_hash_map_at(void)
   hashmap *int_float_hash = hashmap_alloc (hash_int);
   if (int_float_hash == NULL)
     {
-      free_pair_list (&int_float_pairs);
+      free_pair_list (&int_float_pairs, NUM_OF_INT_FLOAT_PAIRS);
       exit (1); // malloc fails.
     }
-  general_at_test (int_float_hash, int_float_pairs); //frees resources
+  general_at_test (int_float_hash, int_float_pairs, NUM_OF_INT_FLOAT_PAIRS);
+  //frees resources
 }
 /***
  *
@@ -210,10 +216,10 @@ void test_hash_map_at(void)
  * @param pairs pair list.
  * performing erases on different scenarios.
  */
-void general_erase_test(hashmap* hash_map, pair** pairs){
+void general_erase_test(hashmap* hash_map, pair** pairs, size_t num_of_pairs){
   size_t initial_hash_size = hash_map->size;
   size_t initial_hash_capacity = hash_map->capacity;
-  for (int i = 0; i <NUM_OF_PAIRS ; ++i)
+  for (size_t i = 0; i <num_of_pairs ; ++i)
     {
       assert((hashmap_erase (hash_map, pairs[i]->key)) == 0);
       //trying to erase from an empty table.
@@ -224,13 +230,13 @@ void general_erase_test(hashmap* hash_map, pair** pairs){
       assert((hashmap_erase (hash_map,NULL))==0);
       //invalid inputs.
     }
-  for (int i = 0; i < NUM_OF_PAIRS; ++i)
+  for (size_t i = 0; i < num_of_pairs; ++i)
     {
       hashmap_insert (hash_map, pairs[i]);
     }
-  initial_hash_size = NUM_OF_PAIRS;
+  initial_hash_size = num_of_pairs;
   initial_hash_capacity = hash_map->capacity;
-  for (int i = 0; i < NUM_OF_PAIRS; ++i)
+  for (size_t i = 0; i < num_of_pairs; ++i)
     {
       assert((hashmap_erase (hash_map, (pairs[i]->key))) == 1);
       initial_hash_size--;
@@ -241,9 +247,9 @@ void general_erase_test(hashmap* hash_map, pair** pairs){
           initial_hash_capacity/=2;
       }
     }
-    //checking size/ capacity through the delition process.
+    //checking size/ capacity through the deletion process.
   hashmap_free(&hash_map);
-  free_pair_list(&pairs);
+  free_pair_list(&pairs, num_of_pairs);
 }
 
 /**
@@ -251,7 +257,7 @@ void general_erase_test(hashmap* hash_map, pair** pairs){
  * If hashmap_erase fails at some points, the functions exits with exit code 1.
  */
 void test_hash_map_erase(void){
-  pair **char_int_pairs = create_char_int_pairs ();
+  pair **char_int_pairs = create_char_int_pairs (NUM_OF_CHAR_INT_PAIRS);
   if (char_int_pairs == NULL)
     {
       exit (1); // malloc fails.
@@ -259,11 +265,12 @@ void test_hash_map_erase(void){
   hashmap *char_int_hash = hashmap_alloc (hash_char);
   if (char_int_hash == NULL)
     {
-      free_pair_list (&char_int_pairs);
+      free_pair_list (&char_int_pairs, NUM_OF_CHAR_INT_PAIRS);
       exit (1); // malloc fails.
     }
-  general_erase_test(char_int_hash, char_int_pairs);//frees resources.
-  pair **int_float_pairs = create_int_float_pairs();
+  general_erase_test(char_int_hash, char_int_pairs, NUM_OF_CHAR_INT_PAIRS);
+  //frees resources.
+  pair **int_float_pairs = create_int_float_pairs(NUM_OF_INT_FLOAT_PAIRS);
   if (int_float_pairs == NULL)
     {
       exit (1); // malloc fails.
@@ -271,10 +278,11 @@ void test_hash_map_erase(void){
   hashmap *int_float_hash = hashmap_alloc (hash_int);
   if (int_float_hash == NULL)
     {
-      free_pair_list (&int_float_pairs);
+      free_pair_list (&int_float_pairs, NUM_OF_INT_FLOAT_PAIRS);
       exit (1); // malloc fails.
     }
-  general_erase_test(int_float_hash, int_float_pairs); //frees resources
+  general_erase_test(int_float_hash, int_float_pairs, NUM_OF_INT_FLOAT_PAIRS);
+  //frees resources
 }
 
 /**
@@ -291,11 +299,13 @@ void test_hash_map_get_load_factor(void){
   dummy_hash->capacity = HASH_MAP_INITIAL_CAP; //returning the value in order
   // to prevent mem leaks.
   hashmap_free (&dummy_hash);
-  pair ** pairs = create_char_int_pairs();
+  pair ** pairs = create_char_int_pairs(NUM_OF_CHAR_INT_PAIRS);
   hashmap* hash_map_test = hashmap_alloc (hash_char);
   size_t size_test = 0;
   size_t capacity_test = HASH_MAP_INITIAL_CAP ;
-  for (int i = 0; i < NUM_OF_PAIRS; ++i)
+  //parallel run of the load factor generator. for each iteration we test
+  //if the values are correct!
+  for (size_t i = 0; i < NUM_OF_CHAR_INT_PAIRS; ++i)
     {
       double load = (double)size_test/capacity_test;
       assert(load == hashmap_get_load_factor (hash_map_test));
@@ -306,7 +316,7 @@ void test_hash_map_get_load_factor(void){
       size_test++;
       //checks load_factor while increasing the size of the table.
     }
-  for (int i = 0; i < NUM_OF_PAIRS; ++i)
+  for (size_t i = 0; i < NUM_OF_CHAR_INT_PAIRS; ++i)
     {
       double load = (double)size_test/capacity_test;
       assert(load == hashmap_get_load_factor (hash_map_test));
@@ -318,32 +328,39 @@ void test_hash_map_get_load_factor(void){
       //checks load_factor while removing pairs from table.
     }
   hashmap_free (&hash_map_test);
-  free_pair_list (&pairs);
+  free_pair_list (&pairs, NUM_OF_CHAR_INT_PAIRS);
 }
 
 /**
  * This function checks the HashMapGetApplyIf function of the hashmap library.
- * If HashMapGetApplyIf fails at some points, the functions exits with exit code 1.
+ * If HashMapGetApplyIf fails at some points, the functions exits with
+ * exit code 1.
+ * we performing 2 tests, for apply if even keys and if char keys are digits.
+ * IMPORTANT NOTE - This test only loops as much as NUM_OF_CHAR_INT_PAIRS
+ * is defined, as well as for the pairs of FLOAT-INT. That is in order
+ * to prevent very long test functions. if needed this can be altered by
+ * changing the macros.
  */
 void test_hash_map_apply_if(){
-  pair** int_float_pairs = create_int_float_pairs();
-  pair** char_int_pairs = create_char_int_pairs();
+  pair** int_float_pairs = create_int_float_pairs(NUM_OF_CHAR_INT_PAIRS);
+  pair** char_int_pairs = create_char_int_pairs(NUM_OF_CHAR_INT_PAIRS);
   hashmap * int_float_hash = hashmap_alloc (hash_int);
   hashmap * char_int_hash = hashmap_alloc (hash_char);
-  for (int i = 0; i < NUM_OF_PAIRS; ++i)
+  for (size_t i = 0; i < NUM_OF_CHAR_INT_PAIRS; ++i)
     {
       hashmap_insert (int_float_hash, int_float_pairs[i]);
       hashmap_insert (char_int_hash, char_int_pairs[i]);
     }
+  //test resoruce creation.
   int num_of_changes_float_int, num_of_changes_char_int;
   num_of_changes_float_int = hashmap_apply_if
       (int_float_hash, is_even, dev_float_value);
   num_of_changes_char_int = hashmap_apply_if
       (char_int_hash, is_digit, double_value);
-  assert(num_of_changes_float_int == NUM_OF_PAIRS/2);
+  assert(num_of_changes_float_int == NUM_OF_CHAR_INT_PAIRS/2);
   //checks num of changes is correct.
   assert(num_of_changes_char_int == NUM_OF_DIGITS);
-  for (int i = 0; i < NUM_OF_PAIRS; ++i)
+  for (size_t  i = 0; i < NUM_OF_CHAR_INT_PAIRS; ++i)
     {
       valueT cur_pair_f_i_val = int_float_pairs[i]->value;
       keyT cur_pair_f_i_key = int_float_pairs[i]->key;
@@ -365,9 +382,10 @@ void test_hash_map_apply_if(){
       else{
           assert(*((int*)cur_pair_c_i_val) == *((int*)hash_val2));
         }
+      //parallel tests for each scenario.
     }
   hashmap_free (&int_float_hash);
   hashmap_free (&char_int_hash);
-  free_pair_list (&int_float_pairs);
-  free_pair_list (&char_int_pairs);
+  free_pair_list (&int_float_pairs, NUM_OF_CHAR_INT_PAIRS);
+  free_pair_list (&char_int_pairs, NUM_OF_CHAR_INT_PAIRS);
 }
